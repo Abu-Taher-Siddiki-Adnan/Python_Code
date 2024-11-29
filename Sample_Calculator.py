a78d4b5c9e4aa682ba69c9636bdddf71af99cb2a
Abu Taher Siddiki Adnan <adnan02802@gamil.com>
2024-11-29 22:23:06 +0600

    Uploaded

---
+++ Sample_Calculator.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} = {a-b}")
+print(f"{a} x {b} = {a*b}")
+print(f"{a} / {b} = {a/b}")
