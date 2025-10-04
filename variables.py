b1c63e0fd6149bd89508c9f4e01d02fe6c221e1e
Abu Taher Siddiki Adnan <adnan02802@gmail.com>
2025-10-04 23:19:38 +0600

    Different Types of Variables

---
+++ variables.py
@@ -0,0 +1,22 @@
+#Number Type Variables
+int_var = 10
+float_var = 20.5
+complex_var = 3 + 4j
+
+print("Integer Variable:", int_var)
+print("Float Variable:", float_var)
+print("Complex Variable:", complex_var)
+
+#String Type Variables
+str_var = "Hello, World!"
+char_var = 'A'
+
+print("String Variable:", str_var)
+print("Character Variable:", char_var)
+
+#List Type Variable
+list_var = ["Alu", "Mula", 10, 20.5]
+
+print("List Variable:", list_var)
+
+
