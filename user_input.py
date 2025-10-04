5663eac0df9b31183fe31359e1ef3fa7da867459
Abu Taher Siddiki Adnan <adnan02802@gmail.com>
2025-10-04 23:32:38 +0600

    Learnt how to take input form user

---
+++ user_input.py
@@ -0,0 +1,7 @@
+user_name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+salary = eval(input("Enter your salary: ")) # Using eval to accept float or int
+
+print("Name:", user_name)
+print("Age:", age)
+print("Salary:", salary)
